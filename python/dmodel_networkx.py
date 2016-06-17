d21af16811b70fa6d9321d306376cc41867b04da
Simon Souv <sst.misc@gmail.com>
2016-06-17 13:00:37 +0200

    Files modified

--- python/dmodel_networkx.py
+++ python/dmodel_networkx.py
@@ -13,33 +13,37 @@ def generate_dmodel() :
     """
     global nx_graph
     nx_graph = nx.MultiDiGraph() #multiDiGraph is an oriented graph that can have several edges between two nodes
-<<<<<<< HEAD
-    st_filename = 'datamodel_mx.txt' #the file opened contains an extract of RDBCNSTR_DBF
-=======
     st_filename = 'datamodel_mx.txt'
->>>>>>> a3303cb8b92e8d91bb1dbc9efff9835e27bbcd82
     try:
         f_dmodel = open(st_filename,'r')
     except:
         print "ERROR - cannot open file ",st_filename
         exit(-1)
-<<<<<<< HEAD
-    
-    #each line of the file contains the information t1;col1;t2;col2
-    st_curline = f_dmodel.readline().strip() #read the first line of the file and
-    while st_curline != '' :
-        l_curline = st_curline.split(';') #convert the string containing the current line into a list
-        s_edgeLabel = l_curline[1]+'-->'+l_curline[3]
-        nx_graph.add_edge(l_curline[0],l_curline[2],label=s_edgeLabel)
-        st_curline = f_dmodel.readline().strip()
-	
-=======
     l_lines = set(f_dmodel.readlines()) # l_ines will contain all lines from the input files without the duplicated ones
     for x in l_lines:
-		l_curline = x.split(';') #convert the string containing the current line into a list
-		s_edgeLabel = l_curline[1]+'--'+l_curline[3]
-		nx_graph.add_edge(l_curline[0],l_curline[2],label=s_edgeLabel)
->>>>>>> a3303cb8b92e8d91bb1dbc9efff9835e27bbcd82
+        l_curline = x.split(';') #convert the string containing the current line into a list
+        s_edgeLabel = l_curline[1]+'--'+l_curline[3]
+        nx_graph.add_edge(l_curline[0],l_curline[2],label=s_edgeLabel)
+
+
+def draw_graph(nx_finalGraph) :
+    """
+    draw_graph will create an image of a graph, displays it on screen but also save it as an image on disk
+    """
+	nx.write_dot(nx_finalGraph,'simon.dot')
+    #pos = nx.circular_layout(nx_finalGraph)
+    #plt.style.use('ggplot')
+    #nx.draw(nx_finalGraph, pos, with_labels=True)
+    #edge_labels = nx.get_edge_attributes(nx_finalGraph,'label')
+    #nx.draw_networkx_edge_labels(nx_finalGraph, pos, edge_labels = edge_labels)
+    #random_value = str(randint(0,1000000))
+    #plt.savefig('path_'+random_value+'.png')
+    #print 'A graphical representation of path between ', start_node, ' and ', end_node, ' is saved under path'+random_value+'.png'
+    #try :
+        #plt.show()
+    #except :
+        #print 'problem showing the graph on screen'
+
 
 def search_shortest (start_node,end_node):
     """
@@ -47,45 +51,21 @@ def search_shortest (start_node,end_node):
     """
     
     try :
-<<<<<<< HEAD
-        path_exists = nx.shortest_path(nx_graph,start_node,end_node) #path_exists will be of type list
-        #print path_exists,type(path_exists)
-        #p = next(path_exists)
-        #print p, type(p)
-        #Let's construct a new DiGraph based on the shortest path
-        nx_finalGraph = nx.DiGraph()
-        edgesAttributes = nx.get_edge_attributes(nx_graph,'label')
-        #print edgesAttributes
-        while len(path_exists) > 1:
-            current = path_exists.pop(0)
-            next = path_exists[0]
-            nx_finalGraph.add_edge(current,next,label=edgesAttributes[current,next,0])		
-        #Let's draw the new graph
-        pos = nx.circular_layout(nx_finalGraph)
-        plt.style.use('ggplot')
-        nx.draw(nx_finalGraph, pos, with_labels=True)
-        edge_labels = nx.get_edge_attributes(nx_finalGraph,'label')
-        nx.draw_networkx_edge_labels(nx_finalGraph, pos, edge_labels = edge_labels)
-        random_value = str(randint(0,1000000))
-        plt.savefig('path_'+random_value+'.png')
-        print 'A graphical representation of path between ', start_node, ' and ', end_node, ' is saved under path'+random_value+'.png'
-        plt.show()
-=======
         path_exists = nx.all_shortest_paths(nx_graph,start_node,end_node) #nx.all_shortest_paths returns all shortest path. This method returns a generator
->>>>>>> a3303cb8b92e8d91bb1dbc9efff9835e27bbcd82
     except nx.NetworkXNoPath:
         print 'There are no paths between', start_node,' and ', end_node
         return(1)
     
     result_graph = nx.MultiDiGraph()
     for p in path_exists: #path_exists is a generator of list, each list is a path between the two nodes. Basically is list is a subgraph
-		result_graph.add_edges_from(nx_graph.subgraph(p).edges(data=True)) #add the sub_graph to the result_graph
+        result_graph.add_edges_from(nx_graph.subgraph(p).edges(data=True)) #add the sub_graph to the result_graph
     
     for i in result_graph.nodes() : # remove self edge, basically a loop on a node
-	    while result_graph.has_edge(i,i) :
-			result_graph.remove_edge(i,i)
-
+        while result_graph.has_edge(i,i) :
+            result_graph.remove_edge(i,i)
     print result_graph.edges(data=True)
+    #draw_graph(result_graph)
+
 
 def search_all_paths (start_node,end_node):
     """
